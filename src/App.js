83c4036274e26d59af3837fd20a974356b8721e3
Luis Rita <luismestrerita@gmail.com>
2016-12-09 15:33:50 +0000

    video 11

--- src/App.js
+++ src/App.js
@@ -1,39 +1,39 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 
-class App extends React.Component {
+class App extends Component {
   constructor() {
     super();
-    this.state = {a: '', b: ''};
+    this.state = { a: '', b: '' };
+    this.update = this.update.bind(this);
   }
 
   update() {
     this.setState({
       a: this.a.refs.input.value,
       b: this.refs.b.value
-    });
+    })
   }
 
   render() {
     return (
       <div>
-        <Input
-          ref={ component => this.a = component}
-          update={this.update.bind(this)}
-        /> {this.state.a}
+        <Input 
+          ref={component => this.a = component} 
+          update={this.update} 
+        />
+        <span>{this.state.a}</span>
         <hr />
-        <input
-          ref="b"
-          type="text"
-          onChange={this.update.bind(this)}
-          /> {this.state.b}
+        <input ref="b" type="text" onChange={this.update} />
+        <span>{this.state.b}</span>
       </div>
-    );
+    )
   }
 }
 
-class Input extends React.Component {
+class Input extends Component {
   render() {
-    return <div><input ref="input" type="text" onChange={this.props.update} /></div>
+    return <input type="text" ref="input" onChange={this.props.update} />
   }
 }
 
